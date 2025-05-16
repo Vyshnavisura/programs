54e7a29c6ad412493de99420fe0fed1fa06c6210
kurraprathyusha <Kurraprathyusha003@gmail.com>
2025-05-16 14:56:48 +0530

    modifeied

--- leapyr.java
+++ leapyr.java
@@ -2,7 +2,7 @@ class Main
 {
 	public static void main(String[] args)
 	{
-		int year=2000;
+		int year=4000;
 		if(year%400==0||year%4==0 && year%100==0)
 			System.out.println("Leap year");
 		else
